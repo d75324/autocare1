20f07cb0500efb21858a88a3ba88f04d96c78695
LVO <112906515+d75324@users.noreply.github.com>
2024-08-28 21:51:20 -0300

    to create groups & models

---
+++ mysite_box/autocare/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from .models import Vehicle, Service
+
+#voy a crear las clases para ver Vehicle y Service desde el admin
+
+class VehicleBE(admin.ModelAdmin):
+	list_display = (
+        'created_at',
+        'owner',
+        'plate',
+        'brand',
+        'moddel',
+        'year',
+        'color',
+        'car_mechanic',
+    )
+	search_fields = (
+		'created_at',
+		'plate',
+		'car_mechanic',
+    )
+	
+admin.site.register(Vehicle, VehicleBE)
+
+class ServiceBE(admin.ModelAdmin):
+	list_display = (
+        'vehicle',
+        'date',
+        'kilometers',
+        'service_type',
+        'coments',
+        'cost',
+    )
+	search_fields = (
+		'vehicle',
+		'date',
+		'service_type',
+    )
+
+admin.site.register(Service, ServiceBE)
