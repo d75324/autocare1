3d5345e19f7ab1f082085a25639445803772d00c
Tadeo Carrier <tadeo@tadeocarrier.com>
2024-08-29 21:17:15 -0300

    agregar url, view y template de cars estático

--- mysite_box/autocare/urls.py
+++ mysite_box/autocare/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, PricingView
+from .views import CarsView, HomeView, PricingView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('pricing/', PricingView.as_view(), name='pricing'),
+    path('cars/', CarsView.as_view(), name='cars'),
 ]
--- mysite_box/autocare/views.py
+++ mysite_box/autocare/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+class CarsView(TemplateView):
+    template_name = 'cars.html'
+
+
 class HomeView(TemplateView):
     template_name = 'home.html'
 
