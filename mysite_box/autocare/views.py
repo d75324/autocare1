8965ee39e346ad7c36911b1e74fecb6a08a6ddd0
Tadeo Carrier <tadeo@tadeocarrier.com>
2024-08-29 21:20:23 -0300

    listar autos de la db en cars views

--- mysite_box/autocare/views.py
+++ mysite_box/autocare/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
 
-class CarsView(TemplateView):
+from autocare.models import Vehicle
+
+class CarsView(ListView):
+    model = Vehicle
     template_name = 'cars.html'
 
 
